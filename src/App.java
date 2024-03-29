17297912f5cf0d14865366c32ecc0e6f98bfa909
AlexeyTen <aten9180929937@gmail.com>
2024-03-30 01:09:53 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import Ui.CalculatorUI;
+
+import java.io.IOException;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        CalculatorUI calculatorUI = new CalculatorUI();
+        calculatorUI.run();
+
+
+    }
+}
